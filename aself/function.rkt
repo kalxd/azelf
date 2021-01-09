66f9a77dcb5c8ae2089f6914da4908c79cce11a8
荀徒之 <kalxd@126.com>
2021-01-09 10:17:31 +0800

    老子终于有了自己的->>啦！

---
+++ aself/function.rkt
@@ -0,0 +1,44 @@
+#lang racket/base
+
+(require racket/stxparam
+         (for-syntax racket/base))
+
+(provide ->>)
+
+(define-syntax-parameter it
+  (λ (stx)
+    (raise-syntax-error (syntax-e stx)
+                        "只能在特定几个宏中使用！")))
+
+(define-for-syntax (has-it? xs)
+  (for/or ([x xs])
+    (cond
+      [(list? x) (has-it? x)]
+      [else (eq? 'it x)])))
+
+(define-syntax (expand-it stx)
+  (define stx-list (cdr (syntax->datum stx)))
+  (if (has-it? stx-list)
+      (syntax-case stx ()
+        [(_ body)
+         #'(λ (arg)
+             (syntax-parameterize ([it (make-rename-transformer #'arg)])
+               body))])
+      (datum->syntax stx
+                     (car stx-list))))
+
+(define-syntax (->> stx)
+  (syntax-case stx ()
+    [(_ a) #'a]
+
+    [(_ a f)
+     #'(let ([g (expand-it f)])
+         (g a))]
+
+    [(_ a f fs ...)
+     #'(let* ([g (expand-it f)]
+              [b (g a)])
+         (->> b fs ...))]))
+
+(define (inc x)
+  (+ 1 x))
