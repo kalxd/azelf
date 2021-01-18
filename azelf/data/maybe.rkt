649d0694de9c713353be16852302531a5986eb93
荀徒之 <kalxd@126.com>
2021-01-18 09:58:17 +0800

    简易Maybe。

--- azelf/data/maybe.rkt
+++ azelf/data/maybe.rkt
@@ -1,4 +1,22 @@
 #lang racket/base
 
+(require racket/contract)
+
+(provide Maybe
+         Maybe?
+         Maybe/c)
+
 (struct Nothing []
   #:transparent)
+
+(struct Just [a]
+  #:transparent)
+
+(struct Maybe [a]
+  #:transparent)
+
+(define Maybe/c
+  (struct/c Maybe (or/c Nothing? Just?)))
+
+(module+ test
+  (require rackunit))
