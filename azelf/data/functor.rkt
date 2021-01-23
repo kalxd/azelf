ef380c1eebb7ed2691dc0dcd65d44b463c6c55f0
荀徒之 <kalxd@126.com>
2021-01-23 09:34:15 +0800

    Functor:fmap + <$>测试。

--- azelf/data/functor.rkt
+++ azelf/data/functor.rkt
@@ -11,8 +11,20 @@
 
 (define-generics Functor
   (fmap f Functor)
+  (<$> f Functor)
   #:defaults
   ([sequence? (define (fmap f Functor)
                 (for/list ([x Functor])
                   (f x)))]
-   [procedure? (define fmap compose)]))
+   [procedure? (define fmap compose)])
+  #:fallbacks
+  [(define/generic self/fmap fmap)
+   (define <$> self/fmap)])
+
+(module+ test
+  (require rackunit)
+  (test-case "<Functor>:maybe + <$>"
+    (check-equal? (list 2 3)
+                  (fmap add1 (list 1 2)))
+    (check-equal? (fmap number? (list 1 "2" 3 "4"))
+                  (<$> number? (list 1 "2" 3 "4")))))
