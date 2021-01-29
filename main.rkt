2b7fc559f3587852d16dd4377702b85d314be3ab
荀徒之 <kalxd@126.com>
2021-01-29 12:10:17 +0800

    function合并到base。

--- main.rkt
+++ main.rkt
@@ -6,8 +6,8 @@
 (export-from racket/base
              racket/contract
 
-             "./function/main.rkt"
-             "./function/pipeline.rkt"
+             "./base/function.rkt"
+             "./base/pipeline.rkt"
 
              "./data/show.rkt"
              "./data/functor.rkt"
