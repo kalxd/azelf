59f0c9745e329309e0169dbb9cd34e7b97c046a3
荀徒之 <kalxd@126.com>
2021-01-30 09:55:08 +0800

    最简单的Foldable。

--- data/foldable.rkt
+++ data/foldable.rkt
@@ -1,5 +1,23 @@
 #lang racket/base
 
-(require racket/generic)
+(require racket/generic
+         "./monoid.rkt"
+         (rename-in racket/base
+                    (foldl list-foldl)))
 
-(define-generics Foldable)
+(provide gen:Foldable
+         Foldable?
+         foldl)
+
+(define (normal-foldl f acc xs)
+  (for/fold ([acc acc])
+            ([x xs])
+    (f acc x)))
+
+(define-generics Foldable
+  (foldl f acc Foldable)
+
+  #:defaults
+  ([list? (define foldl normal-foldl)]
+   [vector? (define foldl normal-foldl)]
+   [string? (define foldl normal-foldl)]))
