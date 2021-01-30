708e96b058102e52335576dbf0621cf43d595372
荀徒之 <kalxd@126.com>
2021-01-30 15:41:34 +0800

    删除pair.rkt。

--- data/show/main.rkt
+++ data/show/main.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 (require racket/generic
-         (only-in "../internal/macro.rkt"
+         (only-in "../../internal/macro.rkt"
                   macro/id
                   macro/remap))
 
