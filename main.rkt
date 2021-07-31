f7be9b7db85fae4e332848e1f4cb6fd2e6c81f01
荀徒之 <kalxd@126.com>
2021-07-31 15:16:57 +0800

    添加nil/do语法。

--- main.rkt
+++ main.rkt
@@ -9,33 +9,5 @@
              "./syntax/mod.rkt"
              "./syntax/pipeline.rkt"
              "./syntax/spread.rkt"
-             "./syntax/curry.rkt")
-#|
-(export-from racket/contract
-             racket/match
-             "./data/eq/main.rkt"
-             "./data/ord/main.rkt"
-             "./data/semigroup/main.rkt"
-             "./data/monoid/main.rkt"
-             "./data/functor/main.rkt"
-             "./data/applicative/main.rkt"
-             "./data/monad/main.rkt"
-             "./data/foldable/main.rkt"
-             "./data/json/main.rkt"
-
-             "./data/maybe/main.rkt"
-             "./data/either/main.rkt")
-
-(require (except-in racket/base
-                    =
-                    <
-                    >
-                    <=
-                    >=
-                    max
-                    min
-                    map
-                    foldl))
-
-(provide (all-from-out racket/base))
-|#
+             "./syntax/curry.rkt"
+             "./syntax/nil.rkt")
---
+++ syntax/nil.rkt
@@ -0,0 +1,49 @@
+#lang racket/base
+
+(require (for-syntax racket/base
+                     syntax/parse))
+
+(define-syntax nil/do
+  (syntax-parser
+    ; 绑定语法。
+    [(_ (var:id (~and (~literal <-)) e:expr) . rest)
+     #'(let ([var e])
+         (and var
+              (nil/do . rest)))]
+
+    ; 定义语法。
+    [(_ ((~literal let) ([x:id e:expr] ...+)) body ...+)
+     #'(let* ([x e] ...)
+         (nil/do body ...))]
+
+    ; 提前中断操作。
+    [(_ ((~literal break) e:expr) . rest)
+     #'e]
+
+    ; 无论如何都不会中断的代码块。
+    [(_ ((~literal !) e:expr) body ...+)
+     #'(begin e (nil/do body ...))]
+
+    ; 最后一句。
+    [(_ e:expr) #'e]
+
+    ; 多条语句
+    [(_ e1:expr e2:expr ...+)
+     #'(and e1 (nil/do e2 ...))]))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "<nil>: nil/do绑定用法"
+    (check-equal? 2 (nil/do (a <- 1) (add1 a)))
+    (check-false (nil/do (a <- #f) (add1 a))))
+
+  (test-case "<nil>: nil/do变量定义"
+    (check-true (nil/do (let ([a #t])) a))
+    (check-true (nil/do (let ([a #f] [b #t])) (or a b))))
+
+  (test-case "<nil>: nil/do提前中断"
+    (check-true (nil/do (a <- 1) (break #t) a)))
+
+  (test-case "<nil>: nil/do自由代码块"
+    (check-true (nil/do (a <- #t) (! (not a)) a))))
