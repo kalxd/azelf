725e965c5833a176ecff2543afb72d81fb60a271
荀徒之 <kalxd@126.com>
2021-09-16 13:58:44 +0800

    给define/match也加个contract。

--- syntax/match.rkt
+++ syntax/match.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 
 (require racket/match
+         racket/contract
          racket/stxparam
+
          (only-in "../internal/keyword.rkt"
                   it)
          (for-syntax racket/base))
@@ -28,3 +30,25 @@
     (check-equal? 3 (f 2))
     (check-equal? 10 (f (list 3 7)))
     (check-equal? 0 (f "hello"))))
+
+(define-syntax (define/contract/match stx)
+  (syntax-case stx ()
+    [(_ (name args ...) contract-body body ...)
+     #'(define/contract (name args ...)
+         contract-body
+         (match* (args ...)
+           body ...))]))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "<match>: define/contract/match"
+    (define/contract/match (list/sum xs)
+      (-> (listof positive?) (or/c zero? positive?))
+      [((list)) 0]
+      [((list a as ...)) (+ a (list/sum as))])
+
+    (check-equal? 10 (list/sum (list 1 2 3 4)))
+    (check-exn exn:fail:contract?
+               (λ ()
+                 (list/sum (list 1 -2 3 -4))))))
