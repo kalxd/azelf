aabb0c7c0152a053730d50c511970fd69f2a24dd
荀徒之 <kalxd@126.com>
2021-09-21 08:12:26 +0800

    增强->>。

--- syntax/pipeline.rkt
+++ syntax/pipeline.rkt
@@ -1,26 +1,59 @@
 #lang racket/base
 
-(require (only-in "../internal/keyword.rkt"
-                  it
-                  expand-it)
-         (for-syntax racket/base))
+(require "../internal/keyword.rkt"
+         (for-syntax racket/base
+                     syntax/parse))
 
 (provide ->>
          <-<)
 
-(define-syntax (->> stx)
-  (syntax-case stx ()
-    [(_ a) #'a]
+; 内部->>最基本实现。
+(define-syntax pipeline->>
+  (syntax-parser
+    ; 更多语句。
+    [(_ var:expr f:expr fs:expr ...+)
+     #'(pipeline->> (pipeline->> var f)
+            fs ...)]
 
-    [(_ a f)
-     #'(let ([g (expand-it f)])
-         (g a))]
+    ; 副作用语句。
+    [(_ var:expr ((~literal !) eff:expr ...+) fs:expr ...)
+     #'(let ([a var])
+         ((expand-it (begin eff ...)) var)
+         (pipeline->> a fs ...))]
 
-    [(_ a f fs ...)
-     #'(let* ([g (expand-it f)]
-              [b (g a)])
-         (->> b fs ...))]))
+    ; 只有两句。
+    [(_ var:expr f:expr)
+     #'((expand-it f) var)]
 
+    ; 最后一句
+    [(_ e:expr) #'e]))
+
+(define-syntax-rule (->> body ...)
+  (break-wrap (pipeline->> body ...)))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "<pipeline>: ->>"
+    ; 常规操作，最低要求。
+    (define a (->> 10
+                   (λ (x) (+ 1 x))
+                   (+ it it)
+                   (- it 1)))
+    (check-equal? 21 a)
+
+    ; 副作用测试。
+    (check-equal? 10
+                  (->> 1
+                       (! (add1 it))
+                       (+ it 9)))
+
+    ; 中断测试。
+    (check-equal? 10
+                  (->> 1
+                       (when (= it 1)
+                         (break 10))
+                       add1))))
 
 (define-syntax (<-< stx)
   (syntax-case stx ()
@@ -32,15 +65,6 @@
          #'(compose fs ...)))]))
 
 (module+ test
-  (require rackunit)
-
-  (test-case "->> pipeline"
-    (define a (->> 10
-                   (λ (x) (+ 1 x))
-                   (+ it it)
-                   (- it 1)))
-    (check-equal? 21 a))
-
   (test-case "<-< compose"
     (define f (<-< number->string (+ 10 it)))
 
