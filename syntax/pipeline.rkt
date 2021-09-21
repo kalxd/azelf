60288b2e9b3d357950299d6f8ee25154b47554da
荀徒之 <kalxd@126.com>
2021-09-21 09:30:55 +0800

    增强<-<，并提供反向参数>->。

--- syntax/pipeline.rkt
+++ syntax/pipeline.rkt
@@ -5,7 +5,8 @@
                      syntax/parse))
 
 (provide ->>
-         <-<)
+         <-<
+         >->)
 
 ; 内部->>最基本实现。
 (define-syntax pipeline->>
@@ -55,17 +56,44 @@
                          (break 10))
                        add1))))
 
+(define-syntax compose>->
+  (syntax-parser
+    ; 只有一条语句。
+    [(_ f:expr)
+     #'(expand-it f)]
+
+    ; 副作用。
+    [(_ f:expr ((~literal !) eff:expr ...+) fs:expr ...+)
+     #'(compose (compose>-> f)
+                (λ (a)
+                  (pipeline->> a
+                               (! eff ...)))
+                (compose>-> fs ...))]
+
+    ; 许多句。
+    [(_ f:expr fs:expr ...)
+     #'(compose (compose>-> fs ...)
+                (compose>-> f))]))
+
+(define-syntax-rule (>-> body ...)
+  (break-wrap (compose>-> body ...)))
+
 (define-syntax (<-< stx)
-  (syntax-case stx ()
-    [(_ f ...)
-     (let* ([fs (syntax->list #'(f ...))]
-            [gs (for/list ([f fs])
-                  `(expand-it ,f))])
-       (with-syntax ([(fs ...) gs])
-         #'(compose fs ...)))]))
+  (define fs (reverse (cdr (syntax->datum stx))))
+  (datum->syntax stx
+                 `(>-> ,@fs)))
 
 (module+ test
-  (test-case "<-< compose"
-    (define f (<-< number->string (+ 10 it)))
+  (test-case "<pipeline>: <-<"
+    ; 最低要求。
+    (define f (<-< number->string
+                   (+ 10 it)))
 
-    (check-equal? "20" (f 10))))
+    (check-equal? "20" (f 10))
+
+    ; 副作用测试。
+    (define g (<-< add1
+                   (! (+ 10 it)
+                      (+ it it))
+                   add1))
+    (check-equal? 3 (g 1))))
