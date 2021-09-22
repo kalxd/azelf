a5f0caf7b8e898b1ce6e97eab9f749cae2387bed
荀徒之 <kalxd@126.com>
2021-09-22 19:58:42 +0800

    删除define/match1，已经有match-lambda了。

--- syntax/match.rkt
+++ syntax/match.rkt
@@ -2,34 +2,10 @@
 
 (require racket/match
          racket/contract
-         racket/stxparam
 
          (only-in "../internal/keyword.rkt"
                   it)
          (for-syntax racket/base))
-
-(define-syntax (define/match1 stx)
-  (syntax-case stx ()
-    [(_ name body ...)
-     #'(define (name x)
-         (syntax-parameterize
-             ([it (make-rename-transformer #'x)])
-           (match x body ...)))]))
-
-(module+ test
-  (require rackunit)
-
-  (test-case "<match>: define/match1"
-    (define/match1 f
-      [1 it]
-      [2 (add1 it)]
-      [(list a b) (+ a b)]
-      [else 0])
-
-    (check-equal? 1 (f 1))
-    (check-equal? 3 (f 2))
-    (check-equal? 10 (f (list 3 7)))
-    (check-equal? 0 (f "hello"))))
 
 (define-syntax (define/contract/match stx)
   (syntax-case stx ()
