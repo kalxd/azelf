ace2de5dff2b6a3262bb51227201e199371caf13
荀徒之 <kalxd@126.com>
2021-09-29 20:28:23 +0800

    补充list测试。

--- ext/list.rkt
+++ ext/list.rkt
@@ -18,6 +18,16 @@
              [y ys])
     (cons x y)))
 
+(module+ test
+  (require rackunit)
+
+  (test-case "<list>: zip"
+    (check-equal? (list (cons 1 1)
+                        (cons 2 2)
+                        (cons 3 3))
+                  (zip (list 1 2 3)
+                       (list 1 2 3)))))
+
 (define/curry (private/traverse acc f xs)
   (match xs
     [(list) acc]
@@ -35,3 +45,16 @@
       (listof any/c)
       (Maybe/c (listof any/c)))
   (private/traverse (Just empty) f xs))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "<list>: traverse"
+    (define (f1 x)
+      (->maybe (and (> 5 x) x)))
+
+    (check-equal? nothing
+                  (traverse f1 (list 3 4 5 6)))
+
+    (check-equal? (Just (list 1 2 3))
+                  (traverse f1 (list 1 2 3)))))
