a285496ee9a4f085481d4339229bd95e9e9bfe6e
荀涧林 <kalxd@126.com>
2022-08-24 20:53:03 +0800

    简单写了点。

---
+++ std/config.rkt
@@ -0,0 +1,54 @@
+#| 一些配置文件处理。 |#
+#lang racket/base
+
+(require "../syntax/pipeline.rkt"
+         "../internal/keyword.rkt"
+         "../syntax/curry.rkt"
+         "../data/maybe.rkt")
+
+(require racket/contract
+         racket/file
+         racket/match
+         racket/generic)
+
+(provide config-ref
+         ConfigRef?
+         config-get
+         config-put)
+
+(struct ConfigRef [ref]
+  #:constructor-name config-ref
+
+  #:methods gen:custom-write
+  [(define/generic self/write-proc write-proc)
+   (define (write-proc c port mode)
+     (match-define (ConfigRef ref) c)
+     (self/write-proc ref port mode))]
+
+  #:guard
+  (λ (ref name)
+    (unless (path-string? ref)
+      (raise-argument-error name
+                            (format "~a不是有效路径" ref)
+                            ref))
+    ref))
+
+(define/curry/contract (config-get key config)
+  (-> symbol? ConfigRef? (Maybe/c any/c))
+  (match-define (ConfigRef ref) config)
+  (maybe-catch
+    (get-preference key
+                    (λ () (->> (format "~a不存在" key)
+                               raise-user-error))
+                    'timestamp
+                    ref)))
+
+(define/curry/contract (config-put key value config)
+  (-> symbol? any/c ConfigRef? void?)
+  (define key-list (list key))
+  (define value-list (list value))
+  (match-define (ConfigRef ref) config)
+  (put-preferences key-list
+                   value-list
+                   #f
+                   ref))
