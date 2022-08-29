38a745d80c36621b5c58847099da410e561a974d
荀涧林 <kalxd@126.com>
2022-08-29 20:40:25 +0800

    添加filter-map。

--- internal/list.rkt
+++ internal/list.rkt
@@ -10,12 +10,24 @@
 
 (provide (all-defined-out))
 
+(define concat (curry/n 2 append))
+
 (define/curry (traverse acc f xs)
   (match xs
     [(list) (Just acc)]
     [(list x xs ...)
      (maybe/do
       (a <- (f x))
-      (let acc- = (->> (list a)
-                       (append acc it)))
-      (traverse acc- f xs))]))
+      (->> (list a)
+           (concat acc)
+           (traverse it f xs)))]))
+
+(define/curry (filter-map acc f xs)
+  (match xs
+    [(list) acc]
+    [(list x xs ...)
+     (match (f x)
+       [(Just x) (->> (list x)
+                      (concat acc)
+                      (filter-map it f xs))]
+       [_ (filter-map acc f xs)])]))
