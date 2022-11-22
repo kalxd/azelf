06e40d4b921c4a23dfc0ac254dbcea2a4062bd07
荀涧林 <kalxd@126.com>
2022-11-22 19:40:56 +0800

    Eq测试用例。

--- test/prelude/eq.rkt
+++ test/prelude/eq.rkt
@@ -7,7 +7,10 @@
    "Eq Typeclass"
    (test-case "eq primitive"
      (check-true (= 1 1))
-     (check-true (= "" "")))))
+     (check-true (= "" "")))
+   (test-case "eq Maybe"
+     (check-true (= nothing nothing))
+     (check-true (= (Just 1) (Just 1))))))
 
 (define task-list
   (list eq-ts))
