93069cdbdbabe88870232e2d6a339f5875e65681
荀涧林 <kalxd@126.com>
2023-06-20 21:13:59 +0800

    v0.6.5

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "azelf")
 (define pkg-desc "超能力工具箱")
-(define version "0.6.4")
+(define version "0.6.5")
 (define pkg-authors '(XGLey))
 
 (define deps '(["base" #:version "8.7"]))
