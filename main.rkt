0edd2ce3ce19469074a2998462e760f2641a608e
荀洪道 <kalxd@126.com>
2025-11-12 14:19:37 +0800

    导出it。

--- main.rkt
+++ main.rkt
@@ -1,9 +1,10 @@
 #lang typed/racket/base
 
 (require "./syntax/pipe.rkt"
+         (only-in "./syntax/it.rkt" it)
          "./primitive/option.rkt")
 
-(provide (all-defined-out)
+(provide it
          (all-from-out typed/racket/base)
          (all-from-out "./syntax/pipe.rkt"
                        "./primitive/option.rkt"))
