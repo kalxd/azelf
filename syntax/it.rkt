a3657f2b1b935cc2e8c6ea3e5ff4c3d2d19a0417
荀洪道 <kalxd@126.com>
2025-11-12 14:02:37 +0800

    添加it关键字。

---
+++ syntax/it.rkt
@@ -0,0 +1,33 @@
+#lang typed/racket/base
+
+(require racket/stxparam
+         (for-syntax racket/base))
+
+(provide it
+         expand-it)
+
+(define-syntax-parameter it
+  (λ (stx)
+    (raise-syntax-error (syntax-e stx)
+                        "只能在特定语法中使用！")))
+
+(define-for-syntax (check-it-syntax syns)
+  (cond
+    [(list? syns)
+     (for/or ([x syns])
+       (check-it-syntax x))]
+    [else (eq? 'it syns)]))
+
+(define-syntax (expand-it stx)
+  (if (check-it-syntax (syntax->datum stx))
+      (syntax-case stx ()
+        [(_ (op ...))
+         #'(λ (arg)
+             (syntax-parameterize ([it (make-rename-transformer #'arg)])
+               #'(op ...)))])
+      (syntax-case stx ()
+        [(_ fn)
+         #'fn])))
+
+(module+ test
+  (expand-it (let ([a 1]) (+ a a))))
