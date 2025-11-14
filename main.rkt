b27db081c7d96f26298117af7dd66712b396fabc
荀洪道 <kalxd@126.com>
2025-11-14 09:27:27 +0800

    重新导出json。

--- main.rkt
+++ main.rkt
@@ -2,9 +2,11 @@
 
 (require "./syntax/pipe.rkt"
          (only-in "./syntax/it.rkt" it)
-         "./primitive/option.rkt")
+         "./primitive/option.rkt"
+         "./primitive/json.rkt")
 
 (provide it
          (all-from-out typed/racket/base)
          (all-from-out "./syntax/pipe.rkt"
-                       "./primitive/option.rkt"))
+                       "./primitive/option.rkt"
+                       "./primitive/json.rkt"))
--- primitive/json.rkt
+++ primitive/json.rkt
@@ -2,19 +2,4 @@
 
 (require typed/json)
 
-(define value : JSExpr 'null)
-
-(: is-null? (-> JSExpr Boolean : 'null))
-(define (is-null? input)
-  (eq? input 'null))
-
-(: print-null (-> 'null Void))
-(define (print-null value)
-  (displayln value))
-
-(module+ main
-  (define value : JSExpr 'null)
-
-  (cond
-    [(is-null? value) (print-null value)]
-    [else (displayln "is not null")]))
+(provide (all-from-out typed/json))
