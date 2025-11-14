5f9a7b9722b541d48bc64cd1e83006f9e4e2c199
荀洪道 <kalxd@126.com>
2025-11-14 18:46:13 +0800

    最基础的json检测。

--- primitive/json.rkt
+++ primitive/json.rkt
@@ -1,5 +1,57 @@
 #lang typed/racket/base
 
-(require typed/json)
+(require typed/json
+         (only-in "./option.rkt"
+                  option/unwrap-exn)
+         "../syntax/pipe.rkt"
+         "../syntax/it.rkt")
 
-(provide (all-from-out typed/json))
+(provide (all-from-out typed/json)
+         exn:fail:json?
+         jstring?
+         jstring!
+         jboolean?
+         jboolean!)
+
+(struct exn:fail:json exn:fail ())
+
+(define-syntax-rule (if-let [body result])
+  (cond
+    [body result]
+    [else #f]))
+
+(: json/unwrap-option
+   (All (a) (-> (Option a) String a)))
+(define (json/unwrap-option ma msg)
+  (option/unwrap-exn ma
+                     (exn:fail:json msg (current-continuation-marks))))
+
+(: jstring? (-> JSExpr (Option String)))
+(define (jstring? value)
+  (if-let ([string? value] value)))
+
+(: jstring! (-> JSExpr String))
+(define (jstring! json)
+  (->> (jstring? json)
+       (json/unwrap-option it
+                           (format "无法将~a转化成string" json))))
+
+(: jboolean? (-> JSExpr (Option Boolean)))
+(define (jboolean? value)
+  (if-let ([boolean? value] value)))
+
+(: jboolean! (-> JSExpr Boolean))
+(define (jboolean! value)
+  (->> (jboolean? value)
+       (let ([msg (format "~a无法转化为boolean" value)])
+         (json/unwrap-option it msg))))
+
+(: jobject? (-> JSExpr (Option HashTableTop)))
+(define (jobject? value)
+  (if-let ([hash? value] value)))
+
+(: jobject! (-> JSExpr HashTableTop))
+(define (jobject! value)
+  (->> (jobject? value)
+       (let ([msg (format "~a无法转化为object" value)])
+         (json/unwrap-option it msg))))
