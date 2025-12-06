bb5a2d061dcdf8748504689f989afd65f8d034ec
荀洪道 <kalxd@126.com>
2025-12-06 19:02:01 +0800

    删除none?和some?；重写option/map。

--- primitive/option.rkt
+++ primitive/option.rkt
@@ -3,9 +3,7 @@
 (require (for-syntax racket/base
                      syntax/parse))
 
-(provide some?
-         none?
-         option/map
+(provide option/map
          option/unwrap-exn
          option/unwrap-error
          option/unwrap
@@ -18,22 +16,12 @@
       [(eq? ma #f) (handle ...)]
       [else ma])))
 
-(: none? (All (a) (-> (Option a) Boolean : #f)))
-(define (none? ma)
-  (eq? #f ma))
-
-(: some? (All (a) (-> (Option a) Boolean : #:+ a)))
-(define (some? ma)
-  (not (none? ma)))
-
 (: option/map
    (All (a b) (-> (Option a)
                   (-> a b)
                   (Option b))))
 (define (option/map ma f)
-  (cond
-    [(none? ma) #f]
-    [else (f ma)]))
+  (and ma (f ma)))
 
 (: option/unwrap-exn
    (All (a) (-> (Option a)
