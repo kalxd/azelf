6d3f907a66d38f08807d3abaf0cc21c7f0b31361
荀洪道 <kalxd@126.com>
2025-12-08 18:46:18 +0800

    重新定义nullable。

--- internal/nullable.rkt
+++ internal/nullable.rkt
@@ -1,22 +1,6 @@
 #lang typed/racket/base
 
-(require "../internal/option.rkt")
-
 (provide Nullable)
 
-(struct nullable/nil ()
-  #:type-name Nullable/Nil)
-
-(struct (A) nullable/some ([value : A])
-  #:type-name Nullable/Some)
-
 (define-type (Nullable A)
-  (U Nullable/Nil (Nullable/Some A)))
-
-(: ->nullable
-   (All (A) (-> A
-                (Nullable A))))
-(define (->nullable value)
-  (if value
-      (nullable/nil)
-      (nullable/some value)))
+  (U 'nil A))
