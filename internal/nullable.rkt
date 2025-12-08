3d1ab3571d5eba72358d07ed078e9a4c47a38b31
荀洪道 <kalxd@126.com>
2025-12-08 18:52:19 +0800

    添加nullable/map。

--- internal/nullable.rkt
+++ internal/nullable.rkt
@@ -1,6 +1,28 @@
 #lang typed/racket/base
 
-(provide Nullable)
+(require racket/match)
+
+(provide Nullable
+         nullable/map)
+
+(struct nullable/nil ()
+  #:type-name Nullable/Nil)
+
+(struct (A) nullable/some ([value : A])
+  #:type-name Nullable/Some
+  #:transparent)
 
 (define-type (Nullable A)
-  (U 'nil A))
+  (U Nullable/Nil (Nullable/Some A)))
+
+(: nullable/map
+   (All (A B)
+        (-> (Nullable A)
+            (-> A B)
+            (Nullable B))))
+(define (nullable/map ma f)
+  (cond
+    [(nullable/nil? ma) ma]
+    [else
+     (match-let ([(nullable/some mb) ma])
+       (nullable/some (f mb)))]))
