3a48fc0985f0a63ab2e64ad96d6863c3e3240067
荀洪道 <kalxd@126.com>
2025-12-08 14:50:54 +0800

    primitive重命名为internal，准备更多的扩展数据结构。

--- main.rkt
+++ main.rkt
@@ -2,11 +2,11 @@
 
 (require "./syntax/pipe.rkt"
          (only-in "./syntax/it.rkt" it)
-         "./primitive/option.rkt"
-         "./primitive/json.rkt")
+         "./internal/option.rkt"
+         "./internal/json.rkt")
 
 (provide it
          (all-from-out typed/racket/base)
          (all-from-out "./syntax/pipe.rkt"
-                       "./primitive/option.rkt"
-                       "./primitive/json.rkt"))
+                       "./internal/option.rkt"
+                       "./internal/json.rkt"))
