305671d4a9d416cfec3f8d035d17d45d508da217
荀洪道 <kalxd@126.com>
2025-12-10 14:11:21 +0800

    添加一大堆辅助方法。

--- internal/nullable.rkt
+++ internal/nullable.rkt
@@ -8,6 +8,10 @@
          match-nullable?
          nullable/map
          nullable/chain
+         nullable/unwrap-exn
+         nullable/unwrap-error
+         nullable/unwrap
+         nullable/unwrap-or
          nullable->option)
 
 (struct nullable/nil ()
@@ -46,6 +50,45 @@
   (match-nullable?
    ma
    [a (f a)]))
+
+(define-syntax-rule (define-nil-function (name args ...) body)
+  (define (name ma args ...)
+    (cond
+      [(nullable/nil? ma) body]
+      [else
+       (match-let ([(nullable/some a) ma])
+         a)])))
+
+(: nullable/unwrap-exn
+   (All (a)
+        (-> (Nullable a)
+            exn
+            a)))
+(define-nil-function (nullable/unwrap-exn e)
+  (raise e))
+
+(: nullable/unwrap-error
+   (All (a)
+        (-> (Nullable a)
+            String
+            a)))
+(define-nil-function (nullable/unwrap-error msg)
+  (raise-user-error msg))
+
+(: nullable/unwrap
+   (All (a)
+        (-> (Nullable a)
+            a)))
+(define-nil-function (nullable/unwrap)
+  (raise-user-error "无法从nil中取值！"))
+
+(: nullable/unwrap-or
+   (All (a)
+        (-> (Nullable a)
+            a
+            a)))
+(define-nil-function (nullable/unwrap-or a)
+  a)
 
 (: nullable->option
    (All (A)
