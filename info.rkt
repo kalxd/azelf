28326b69e7c063d47c2749691affcc776d76e11d
荀洪道 <kalxd@126.com>
2025-12-11 14:09:58 +0800

    v0.7.2

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "azelf")
 (define pkg-desc "超能力工具箱")
-(define version "0.7.0")
+(define version "0.7.2")
 (define pkg-authors '(XGLey))
 
 (define deps '(["base" #:version "8.16"]))
